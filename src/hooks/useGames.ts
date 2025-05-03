7350fab37aaa3e0d2aaf3ac83805bd2658658f38
ddkeeper <you@example.com>
2025-05-03 23:24:10 +0800

    Create a custom hook to fetch the game

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,37 @@
+import apiClient from "@/services/api-client";
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        /* 
+        AbortController 被用来控制 fetch 请求，以便能够提前终止请求;
+        例如，用户在请求完成前导航离开了当前页面.
+        */
+        const controller = new AbortController();
+        apiClient
+            .get<FetchGamesResponse>("/games", { signal: controller.signal })
+            .then((res) => setGames(res.data.results))
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message);
+            });
+        return () => controller.abort();
+    }, []);
+
+    return { games, error };
+};
+export default useGames;
