5386dbdd5863e4ad41397e35ef4a18cfbfee8169
ddkeeper <you@example.com>
2025-05-05 21:23:27 +0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
 // 获取游戏图片清晰度更小的版本以加快获取与显示速度
+import noImage from "../assets/no-image-placeholder.jpg";
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return "";
+    if (!url) return noImage;
     const target = "media/";
     const index = url.indexOf(target) + target.length;
     const res = url.slice(0, index) + "crop/600/400/" + url.slice(index);
